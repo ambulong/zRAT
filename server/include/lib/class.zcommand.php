3bb6b24cfb82b61c01fe8ab4a523c793d1e64a6b
Ambulong <zeng.ambulong@gmail.com>
2014-11-30 12:03:45 +0800

    update db structure

--- server/include/lib/class.zcommand.php
+++ server/include/lib/class.zcommand.php
@@ -1,1 +1,34 @@
 <?php
+/**
+ * 命令相关操作
+ * 
+ */
+class zCommand {
+	
+	private $dbh = NULL;
+	
+	public function __construct() {
+		$this->dbh = $GLOBALS ['z_dbh'];
+	}
+	
+	
+	/*
+	 * 获取未执行命令列队里的第一条
+	 */
+	 public function getCommand($id) {
+		global $table_prefix;
+		$id = intval ( $id );
+		if (!$this->isExistID ( $id )) {
+			return FALSE;
+		}
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}commands WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $id );
+			$sth->execute ();
+			$result = $sth->fetch ( PDO::FETCH_ASSOC );
+			return array();
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+}
--- server/include/lib/class.zhook.php
+++ server/include/lib/class.zhook.php
@@ -1,1 +1,24 @@
 <?php
+/**
+ * 获取命令
+ * 
+ */
+class zHook {
+	private $sid = null;
+	public function __construct() {
+		$this->sid = isset($_POST['sid'])?$_POST['sid']:"";
+	}
+	
+	public function init(){
+		$sid = $this->sid;
+		$host_obj = new zHost();
+		if($host_obj->isExistSID($sid)){
+			$id = $host_obj->getID($sid);
+			$data = (new zCommand())->getCommand($id);
+			resp(1, $data['id'], $data['command'], $data['data']);
+		}else{
+			header ( "HTTP/1.0 404 Not Found" );
+			exit ();
+		}
+	}
+}
