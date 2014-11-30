e08c2946847c6f76c8451678d30af58542cde9fb
Ambulong <zeng.ambulong@gmail.com>
2014-11-30 12:32:58 +0800

    fix class.zhook (data is nul)

--- server/include/lib/class.zhook.php
+++ server/include/lib/class.zhook.php
@@ -17,7 +17,10 @@ class zHook {
 			$data = (new zCommand())->getCommand($id);
 			if(is_json($data['data']))
 				$data['data'] = json_decode($data['data'], true);
-			resp(1, $data['sid'], $data['command'], $data['data']);
+			if(isset($data['id']))
+				resp(1, $data['sid'], $data['command'], $data['data']);
+			else 
+				resp(0);
 		}else{
 			header ( "HTTP/1.0 404 Not Found" );
 			exit ();
