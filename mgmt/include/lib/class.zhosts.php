559df626b4f326962602745992a3495dbf846922
Ambulong <zeng.ambulong@gmail.com>
2015-02-07 01:41:27 +0800

    implement mgmt class.zhosts

---
+++ mgmt/include/lib/class.zhosts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 主机列表操作
+ * 
+ */
+class zHost {
+	
+	private $dbh = NULL;
+	
+	public function __construct() {
+		$this->dbh = $GLOBALS ['z_dbh'];
+	}
+	
+	public function getList($offset, $rows) {
+		global $table_prefix;
+		$offset = intval ( $offset );
+		$rows = intval($rows);
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}hosts limit {$offset},{$rows} " );
+			$sth->execute ();
+			$result = $sth->fetch ( PDO::FETCH_ASSOC );
+			return $result;
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	}
