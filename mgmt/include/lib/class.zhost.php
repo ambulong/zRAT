cbf596f93d678d0d4b7d6a7dffd6a340ada8fece
Ambulong <zeng.ambulong@gmail.com>
2015-02-07 01:38:56 +0800

    implement mgmt class.zhost

---
+++ mgmt/include/lib/class.zhost.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * 主机操作
+ * 
+ */
+class zHost {
+	
+	private $dbh = NULL;
+	
+	public function __construct() {
+		$this->dbh = $GLOBALS ['z_dbh'];
+	}
+	
+	public function isExistID($id) {
+		$id = intval ( $id );
+		global $table_prefix;
+		try {
+			$sth = $this->dbh->prepare ( "SELECT count(*) FROM {$table_prefix}hosts WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $id );
+			$sth->execute ();
+			$row = $sth->fetch ();
+			if ($row [0] > 0) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	
+	public function getDetail($id) {
+		global $table_prefix;
+		$id = intval ( $id );
+		if (!$this->isExistID ( $id )) {
+			return FALSE;
+		}
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}hosts WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $id );
+			$sth->execute ();
+			$result = $sth->fetch ( PDO::FETCH_ASSOC );
+			return $result;
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+}
