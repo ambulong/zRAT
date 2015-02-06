69df9b172e39149bb7d03a48a2d755b6a9ace2fd
Ambulong <zeng.ambulong@gmail.com>
2015-02-06 22:00:00 +0800

    implement mgmt login

--- mgmt/include/action/login.php
+++ mgmt/include/action/login.php
@@ -13,6 +13,14 @@ if($username == "" || $password == "") {
 
 $user = new zUser();
 
-if (! $user->validatePassword($username, $password)) {
-	
+if (!$user->validatePassword($username, $password)) {
+	resp(0, array("token" => ""));
 }
+
+$token = $user->login($username);
+if($token)
+	resp(1, array("token" => $token));
+else 
+	resp(0, array("token" => ""));
+
+
