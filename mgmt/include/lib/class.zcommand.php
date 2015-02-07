491b3c60594e9274f6308f51f3dc119ca628f490
Ambulong <zeng.ambulong@gmail.com>
2015-02-07 16:00:08 +0800

    add mgmt class.zcommand

---
+++ mgmt/include/lib/class.zcommand.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * 命令操作
+ * 
+ */
+class zCommand {
+	
+	private $dbh = NULL;
+	
+	public function __construct() {
+		$this->dbh = $GLOBALS ['z_dbh'];
+	}
+	
+	public function add($hid, $command, $data, $timestamp) {
+		
+	}
+	
+	public function getResp($cid) {
+		
+	}
+	
+	public function getCommandList($offset = 0, $rows = 13, $hid = 0, $status = null) {
+		
+	}
+	
+	public function isExistID($id) {
+		$id = intval ( $id );
+		global $table_prefix;
+		try {
+			$sth = $this->dbh->prepare ( "SELECT count(*) FROM {$table_prefix}commands WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $id );
+			$sth->execute ();
+			$row = $sth->fetch ();
+			if ($row [0] > 0) {
+				return TRUE;
+			} else {
+				return FALSE;
+			}
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	
+	public function getDetail($id) {
+		global $table_prefix;
+		$id = intval ( $id );
+		if (!$this->isExistID ( $id )) {
+			return FALSE;
+		}
+		try {
+			$sth = $this->dbh->prepare ( "SELECT * FROM {$table_prefix}commands WHERE `id` = :id " );
+			$sth->bindParam ( ':id', $id );
+			$sth->execute ();
+			$result = $sth->fetch ( PDO::FETCH_ASSOC );
+			return $result;
+		} catch ( PDOExecption $e ) {
+			echo "<br>Error: " . $e->getMessage ();
+		}
+	}
+	
+	
+}
