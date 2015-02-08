a937a63e359f42b8b4933ce773ceef08c689d2e6
Ambulong <zeng.ambulong@gmail.com>
2015-02-08 19:10:23 +0800

    update client linux

--- client/linux/main.cpp
+++ client/linux/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+string url = "localhost";   //上线地址
+int times = 3000;           //请求周期 ms
+string sid = "";
+string key = "";
+
+/**
+* @brief get randon string
+*/
+string getSalt(int len = 100);
+
+bool chkEnv();
+bool reg();
+string getCommand();
+bool sendResult(string cid, int status, string data);
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    srand(time(NULL));
+    sid = getSalt(123);
+    cout << "sid:" << sid << endl;
     return 0;
 }
+
+string getSalt(int len)
+{
+    int i;
+    char str[len + 1];
+    for(i=0;i<len;++i)
+        str[i]='A'+rand()%26;
+    str[++i]='\0';
+    return str;
+}
+
+
